96bec54e6d5d6c48615e45808e6267c601611116
haein <sodychoe@icloud.com>
2022-12-08 21:19:23 +0900

    ddd

---
+++ _pages/papers.md
@@ -0,0 +1,16 @@
+---
+title: "Papers"
+permalink: /categories/papers/
+layout: category
+author_profile: true
+taxonomy: papers
+---
+
+[//]: # ( 이 페이지는 단일 카테고리의 대표 포스트임)
+
+읽었던 논문을 정리합니다
+
+# 관심 분야 
+
+1. Causal Inference(인과추론)
+2. NLP(자연어 처리)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning(2015)",
+        "excerpt":"아름다운 시작  ","categories": ["papers"],
+        "tags": ["papers"],
+        "url": "/papers/first-post/",
+        "teaser": null
+      }]
