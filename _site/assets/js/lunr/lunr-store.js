ca12850d382c30e85717d6ad6fce3f4cfd6556e0
haein <sodychoe@icloud.com>
2022-12-10 17:28:04 +0900

    ddd

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning(2015)",
-        "excerpt":"아름다운 시작  ","categories": ["papers"],
-        "tags": ["papers"],
+        "title": "Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning(ICML 2015)",
+        "excerpt":"Introduction   Related Research   Dropout as a Bayesian Approximation   Obtaining Model Uncertainty   Experiments   Conclusions and Future Research  ","categories": ["papers"],
+        "tags": ["papers","uncertainty"],
         "url": "/papers/first-post/",
+        "teaser": null
+      },{
+        "title": "Causal Understanding of Fake News Dissemination on Social Media",
+        "excerpt":"INTRODUCTION   RELATED WORK   Fake News Detection   Propensity Scoring Methods   PROBLEM STATEMENT   THE PROPOSED FRAMEWORK   Modeling Fake News Dissemination   Learning Unbiased Sharing Behavior   Identifying Causal User Attributes   EMPIRICAL EVALUATION   Experimental Setup   Evaluation on Fake News Dissemination   Evaluation on Identifying Causal User Attributes   DISCUSSION  ","categories": ["papers"],
+        "tags": ["papers","causalinference"],
+        "url": "/papers/causalfakenews-post/",
         "teaser": null
       }]
