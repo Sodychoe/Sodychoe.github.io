d2491606ef0dab3755a7ac1335f32eac6f838e88
haein <sodychoe@icloud.com>
2022-12-19 00:23:02 +0900

    d

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning(ICML 2015)",
+        "title": "Dropout as a Bayesian Approximation: Representing Model Uncertainty in Deep Learning",
         "excerpt":"Introduction   Related Research   Dropout as a Bayesian Approximation   Obtaining Model Uncertainty   Experiments   Conclusions and Future Research  ","categories": ["papers"],
         "tags": ["papers","uncertainty"],
         "url": "/papers/first-post/",
@@ -9,5 +9,11 @@ var store = [{
         "excerpt":"INTRODUCTION   RELATED WORK   Fake News Detection   Propensity Scoring Methods   PROBLEM STATEMENT   THE PROPOSED FRAMEWORK   Modeling Fake News Dissemination   Learning Unbiased Sharing Behavior   Identifying Causal User Attributes   EMPIRICAL EVALUATION   Experimental Setup   Evaluation on Fake News Dissemination   Evaluation on Identifying Causal User Attributes   DISCUSSION  ","categories": ["papers"],
         "tags": ["papers","causalinference"],
         "url": "/papers/causalfakenews-post/",
+        "teaser": null
+      },{
+        "title": "Structural Causal Model",
+        "excerpt":"인과 추론을 위한 프레임워크를 먼저 정의하려고 한다 이 포스트는 CAUSAL INFERENCE IN STATISTICS: A PRIMER , Pearl et al.(2016) Elements of Causal Inference : Foundations and Learning Algorithms , Peters et al.(2017) 을 참고하여 작성하였다 1. SCM 의 정의 Structural Causal Model(SCM) 은 몆 가지 적절한 가정을 도입하여 주어진 데이터(observational...","categories": ["study"],
+        "tags": ["study","causal","scm","causaleffects"],
+        "url": "/study/scm1-post/",
         "teaser": null
       }]
